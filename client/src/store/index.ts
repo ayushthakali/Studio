f17b62e9e03000c331efb0b018fca1f081e95d87
AyushThakali <thakaliaayush@gmail.com>
2026-06-08 20:55:08 +0545

    redux store setup

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -2,13 +2,15 @@ import authReducer from "@/features/auth/authSlice";
 import { apiService } from "@/services/api";
 import { configureStore } from "@reduxjs/toolkit";
 
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    [apiService.reducerPath]: apiService.reducer,
-  },
-  middleware: (gDM) => gDM().concat(apiService.middleware),
-});
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export const makeStore = () => {
+  return configureStore({
+    reducer: {
+      auth: authReducer,
+      [apiService.reducerPath]: apiService.reducer,
+    },
+    middleware: (gDM) => gDM().concat(apiService.middleware),
+  });
+};
+export type AppStore = ReturnType<typeof makeStore>;
+export type RootState = ReturnType<AppStore["getState"]>;
+export type AppDispatch = AppStore["dispatch"];
